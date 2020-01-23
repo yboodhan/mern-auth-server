56c2a504d9dc6fa1f104a744bb33ea21b10bb793
yboodhan <yboodhan@gmail.com>
2020-01-23 14:31:36 -0800

    Added bcrypt for user model password

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+let bcrypt = require('bcryptjs')
 let mongoose = require('mongoose')
 
 let userSchema = new mongoose.Schema({
@@ -23,9 +24,22 @@ let userSchema = new mongoose.Schema({
 })
 
 // Use bcrypt to hash password
+userSchema.pre('save', function (next) {
+  this.password = bcrypt.hashSync(this.password, 12)
+  next()
+})
 
 // Ensure that password doesn't get sent with the rest of the data
+userSchema.set('toJSON', {
+  transform: (doc, user) => {
+    delete user.password
+    return user
+  }
+})
 
 // Create a helper function to compare the password hashes
+userSchema.methods.isValidPassword = function(typedPassword) {
+  return bcrypt.compareSync(typedPassword, this.password)
+}
 
 module.exports = mongoose.model('User', userSchema)
