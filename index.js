a14443dbb6cb604b77a5eeee02ddbd0383aa31e9
yboodhan <yboodhan@gmail.com>
2020-01-23 16:05:59 -0800

    Protected /auth route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Require needed packages
+require('dotenv').config()
 let cors = require('cors')
 let express = require('express')
+let expressJwt = require('express-jwt')
 let morgan = require('morgan')
 let rowdyLogger = require('rowdy-logger')
 
@@ -15,7 +17,13 @@ app.use(express.urlencoded({ extended: false })) // Accept form data
 app.use(express.json()) // Accept data from fetch (or any AJAX (fetch, ajax, axios) call)
 
 // Routes
-app.use('/auth', require('./controllers/auth'))
+app.use('/auth', expressJwt({ secret: process.env.JWT_SECRET })
+  .unless({ // defines exceptions to the rule, without it the entire /auth is locked
+    path: [
+      { url : '/auth/login', methods: ['POST'] },
+      { url: 'auth/signup', methods: ['POST'] }
+    ]
+  }), require('./controllers/auth'))
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Not Found' })
